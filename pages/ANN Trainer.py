c69b28d2d62f05a9d2365ec7e94ac2e84f85ce44
Abhilash Puthanveettil Madathil <abhilash.p-m@strath.ac.uk>
2025-06-20 09:52:50 +0100

    Updated ANN sample download code and file references

--- pages/ANN Trainer.py
+++ pages/ANN Trainer.py
@@ -2,5 +2,11 @@ import ann_gui
 import streamlit as st
 
 st.sidebar.title("📥 Sample Data")
-with open("sample_data/ann_sample.csv", "rb") as f:
-    st.sidebar.download_button("Download ANN Sample", f, file_name="ann_sample.csv")
+
+# Download button for ANN_trainX.csv
+with open("sample_data/ANN_trainX.csv", "rb") as f:
+    st.sidebar.download_button("Download ANN Training Input (X)", f, file_name="ANN_trainX.csv")
+
+# Download button for ANN_trainY.csv
+with open("sample_data/ANN_trainY.csv", "rb") as f:
+    st.sidebar.download_button("Download ANN Training Output (Y)", f, file_name="ANN_trainY.csv")
--- pages/LSTM Forecaster.py
+++ pages/LSTM Forecaster.py
@@ -2,5 +2,5 @@ import lstm_gui
 import streamlit as st
 
 st.sidebar.title("📥 Sample Data")
-with open("sample_data/lstm_sample.csv", "rb") as f:
-    st.sidebar.download_button("Download LSTM Sample", f, file_name="lstm_sample.csv")
+with open("sample_data/Timeseries_train.csv", "rb") as f:
+    st.sidebar.download_button("Download LSTM Sample", f, file_name="Timeseries_train.csv")
