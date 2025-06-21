b5bb81be7226daa5d33f48fddef103275683f0cb
Abhilash Puthanveettil Madathil <abhilash.p-m@strath.ac.uk>
2025-06-21 09:21:18 +0100

    Fix paths, switch to CSV, add logos

--- pages/ANN Trainer.py
+++ pages/ANN Trainer.py
@@ -4,11 +4,11 @@ import ann_gui
 # Sidebar sample data section
 st.sidebar.title("📥 Sample Data")
 # Download button for ANN_trainX.csv
-with open("sample_data/ann_sample.csv", "rb") as f:
+with open("sample_data/NN_trainX.csv", "rb") as f:
     st.sidebar.download_button("Download ANN Training Input (X)", f, file_name="ANN_trainX.csv")
 
 # Download button for ANN_trainY.csv
-with open("sample_data/ann_sample_y.csv", "rb") as f:
+with open("sample_data/ANN_trainY.csv", "rb") as f:
     st.sidebar.download_button("Download ANN Training Output (Y)", f, file_name="ANN_trainY.csv")
 
 # Run the actual ANN GUI
--- pages/LSTM Forecaster.py
+++ pages/LSTM Forecaster.py
@@ -4,7 +4,7 @@ import lstm_gui
 # Sidebar sample data section
 st.sidebar.title("📥 Sample Data")
 st.sidebar.markdown("Download a sample LSTM time series file:")
-with open("sample_data/lstm_sample.csv", "rb") as f:
-    st.sidebar.download_button("Download LSTM Sample", f, file_name="lstm_sample.csv")
+with open("sample_data/Timeseries_train.csv", "rb") as f:
+    st.sidebar.download_button("Download LSTM Sample", f, file_name="Timeseries_train.csv")
 # Launch the actual GUI
 lstm_gui.run_lstm_gui()
